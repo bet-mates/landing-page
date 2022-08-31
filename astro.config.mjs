7bde1949d06b0b1e891825f3429b3bfe07e02db7
Walter Souto <wsouto@gmail.com>
2022-08-30 22:08:19 -0300

    Add action to deploy the page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  integrations: [tailwind(), alpinejs()],
+
+  site: 'https://bet-mates.github.io',
+  // base: '/landing-page',
 });
