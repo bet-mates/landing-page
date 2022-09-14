e2f69c58bbd349a643b4649993a6c42616c6c5da
Walter Souto <wsouto@gmail.com>
2022-09-14 00:16:41 -0300

    Adds Tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [],
-
+  integrations: [tailwind()],
   // https://docs.astro.build/en/guides/deploy/github/
-  site: 'https://www.betmates.app',
+  site: 'https://www.betmates.app'
 });
